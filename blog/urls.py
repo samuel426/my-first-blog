49f2cc68d1b88f3c74f1f417e1ad5f4ec2ad41cd
samuel426 <samuel2626@naver.com>
2024-10-07 16:32:12 +0000

    Added views to create/edit blog post inside the site.

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,10 @@
-from django.conf.urls import url  # url로 수정
+from django.conf.urls import url  # url로 변경
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'),  # 빈 경로를 정규 표현식으로 설정
+    url(r'^$', views.post_list, name='post_list'),  # 빈 경로
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),  # <int:pk> → 정규 표현식으로 변경
+    url(r'^post/new/$', views.post_new, name='post_new'),  # 새 글 작성 경로
+    url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),  # 수정 경로
 ]
 
